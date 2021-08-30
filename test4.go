06264fa3fcc65a0c452e53932de831faab6b5140
ruoyunbai <56343340+ruoyunbai@users.noreply.github.com>
2021-08-30 15:31:33 +0800

    Update test4.go

--- test4.go
+++ test4.go
@@ -34,7 +34,7 @@ func main() {
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
 	})
-	err := http.ListenAndServe("localhost:8077", nil)
+	err := http.ListenAndServe(":8077", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
